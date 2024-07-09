1f9a4c267dd889e5f834d9de1373b47f24197d2c
Rna Remote <rmlswin@gmail.com>
2024-07-09 03:09:32 +0000

    Fix script file for user_data ec2

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#! /bin/bash
 sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker
 sudo yum install git -y
-sudo mkdir app && cd app/
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/rmsghost/projetct_stackobservability.git
+sudo git clone https://github.com/rmsghost/projetct_stackobservability.git /home/app/projetct_stackobservability
+sudo docker-compose -f /home/app/projetct_stackobservability/compose.yaml up -d --build
+sudo cp /home/app/projetct_stackobservability/daemon.json /etc/docker/daemon.json
+sudo systemctl start docker
