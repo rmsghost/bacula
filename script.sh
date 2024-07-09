ae13d2e70506b48570be8957b16d71a7bf75547a
Rna Remote <rmlswin@gmail.com>
2024-07-09 02:28:00 +0000

    Fix deploy ec2

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@ sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker
 sudo yum install git -y
+sudo mkdir app && cd app/
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo git clone https://github.com/rmsghost/projetct_stackobservability.git
